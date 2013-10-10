4532aed0e46ebea9a7df4f9a16b81bc5e8213938
ceceiliaallwein <ceceilia.allwein@gmail.com>
2013-10-10 12:57:33 -0400

    hello world commits

--- swampy-package/swampy/sib_polygon.py
+++ swampy-package/swampy/sib_polygon.py
@@ -1,12 +1,14 @@
 # Polygon excercise from Week 0
 
-# Name:
+# Name: Ceceilia Allwein
+
+# Note: Function calls commented out in order to select 
 
 from TurtleWorld import * 		
 
 world = TurtleWorld()			
 bob = Turtle()				
-
+bob.delay = .1
 
 
 # This is where you put code to move bob
@@ -14,11 +16,13 @@ bob = Turtle()
 import math 
 
 def square (t, l):
+	'''moves turtle one line segment
+	and pivots right the default angle'''
 	for i in range (4):
 		fd (t, l)
 		rt (t)
 
-square (bob, 30)
+#square (bob, 30)
 
 def square_ref (t, n, l, a):
 	''' draws n line segments + turns with 
@@ -30,44 +34,46 @@ def square_ref (t, n, l, a):
 def polygon (t, n, l): 
 	''' determines the angle of the turn 
 	based on the given number of sides'''
-	a = 360 / n 
+	a = 360 / n
 	square_ref (t, n, l, a)
 
 #polygon (bob, 10, 8)
 
 def circle (t, r):
-	''' relates the number of sides and the length 
-	of those sides to the circumference of the 
-	circle with a given radius '''
+	'''draws an approximate circle; relates 
+	the number of sides and the length of 
+	those sides to the circumference of the 
+	circle with a given radius'''
 	circumference = 2 * math.pi * r
-	n = int (circumference / 3)
+	n = r
 	l = circumference / n 
-	polygon (t, l, n)
+	polygon (t, n, l)
 
-#circle (bob, 35)
+#circle (bob, 10)
 
+def line_subtend (turtle, no_sides, seg_length, angle):
+	''' draws line segments + turns similar
+	to square or square_ref '''
+	for i in range (no_sides):
+		fd (turtle, seg_length)
+		rt (turtle, angle)
 
-def arc (t, r, theta):
- 	arc_length = 2 * math.pi * r * theta / 360
-	n = int (arc_length / 3)
-	l = arc_length / n
-	a = int (theta / n)
-	square_ref (t, l, n, a)
+def polygon (turtle, no_sides, seg_length): 
+	''' determines the angle of the turn 
+	based on the given number of sides'''
+	angle = 360.0 / no_sides
+	line_subtend (turtle, no_sides, seg_length, angle)
 
-	# making a slight left turn before starting reduces
-    # the error caused by the linear approximation of the arc
-    #lt(t, step_angle/2)
-    #polyline(t, n, step_length, step_angle)
-    #rt(t, step_angle/2)
+def arc (turtle, radius, angle):
+	'''defines 
+	''' 
+	circumference = 2 * math.pi * radius
+	seg_length = circumference * angle / 360
+	no_sides = int(seg_length / radius)
+	draw_length = float(seg_length / no_sides)
+	angle_subtend_deg = angle / no_sides
+	line_subtend (turtle, no_sides, draw_length, angle_subtend_deg)
 
-
-arc (bob, 50, 30)
-
-def circle_ref (t, r):
-	arc (t, r, 360)
-
-circle_ref()
-
-
+arc(bob, 10, 30)
 
 wait_for_user()					
